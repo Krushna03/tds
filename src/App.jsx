d3f72abd30108f9aac380f62cdc9617d5b653f01
Krushna03 <krushnasakhare965@gmail.com>
2025-08-22 21:00:50 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LandingPage from './Pages/LandingPAge';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
 
