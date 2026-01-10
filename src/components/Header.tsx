3de7b0af951813034b9fa83b5e1fa8c5879e6327
Krushna Sakhare <95876502+Krushna03@users.noreply.github.com>
2026-01-11 00:26:59 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-0">
           <div className="flex justify-between items-center py-4 md:py-1">
             <div className="flex items-center">
-              <img src="logo-1.png" alt="tds-logo" className="h-[62px]" />
+              <img src="avenue-codeforge.jpg" alt="tds-logo" className="h-[62px]" />
             </div>
 
             {/* Desktop Navigation */}
